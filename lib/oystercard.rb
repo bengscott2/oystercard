477b729147abfee056aed8eeecdd630601ca31af
Ben Scott <bengscott2@gmail.com>
2019-06-03 15:33:55 +0100

    Added exception for exceeding limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class Oystercard
   DEFAULT_BALANCE = 0
+  DEFAULT_LIMIT = 90
   attr_reader :balance
 
   def initialize(balance = DEFAULT_BALANCE)
@@ -7,7 +10,11 @@ class Oystercard
   end
 
   def top_up(amount)
+    raise "Top up amount would exceed #{DEFAULT_LIMIT} max balance" if exceed?(amount)
     @balance += amount
   end
 
+  def exceed?(amount)
+    amount + @balance > DEFAULT_LIMIT
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 require 'oystercard'
 
 describe Oystercard do
- it 'checks that a new card has a balance of zero' do
-   expect(subject.balance).to eq(0)
- end
+  it 'checks that a new card has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
 
- it 'increments the balance by 20' do
-  expect { subject.top_up(20) }.to change { subject.balance }.by(20)
-end
+  it 'increments the balance by 20' do
+    expect { subject.top_up(20) }.to change { subject.balance }.by(20)
+  end
 
+  it 'raises an erorr if top up amount goes beyond limit' do
+    expect { subject.top_up(Oystercard::DEFAULT_LIMIT + 1) }.to raise_error("Top up amount would exceed #{Oystercard::DEFAULT_LIMIT} max balance")
+  end
 end
