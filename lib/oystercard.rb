3dae2fdc448707665a6dcf4035b073448ac49c7f
Ben Scott <bengscott2@gmail.com>
2019-06-03 16:24:29 +0100

    Added tap in/ out methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,6 +7,7 @@ class Oystercard
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -16,6 +17,18 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,23 +3,46 @@
 require 'oystercard'
 
 describe Oystercard do
+
   it 'checks that a new card has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
 
   context 'tests around topping up' do
+
     it 'increments the balance by 20' do
       expect { subject.top_up(20) }.to change { subject.balance }.by(20)
     end
-  
+
     it 'raises an erorr if top up amount goes beyond limit' do
       expect { subject.top_up(Oystercard::DEFAULT_LIMIT + 1) }.to raise_error("Top up amount would exceed #{Oystercard::DEFAULT_LIMIT} max balance")
-    end  
+    end
+
   end
 
   context 'tests that money can be deducted' do
+
     it 'decrements the balance by 20' do
       expect { subject.deduct(20) }.to change {subject.balance }.by(-20)
-    end  
+    end
+
+  end
+
+  context 'testing touch in/ touch out and card state' do
+
+    it 'initial in_journey state is false' do
+      new_card = Oystercard.new
+      expect(new_card).not_to be_in_journey
+    end
+
+    it 'changes state of card when touched in' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'changes state when touched out' do
+      expect(subject).not_to be_in_journey
+    end
+
   end
 end
