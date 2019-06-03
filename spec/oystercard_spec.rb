4386778cdc1c0c3fb9f864a1acdf60b36165a056
Alexis Josephides <ajosephides@gmail.com>
2019-06-03 15:15:41 +0100

    added test and method for topping up card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
  it 'checks that a new card has a balance of zero' do
    expect(subject.balance).to eq(0)
  end
+
+ it 'increments the balance by 20' do
+  expect { subject.top_up(20) }.to change { subject.balance }.by(20)
+end
+
 end
